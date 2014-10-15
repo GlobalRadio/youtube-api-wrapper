67a528a7907f0d9f04f1423872732efb9fa1dad6
Alessandro De Noia <alessandro.denoia@gmail.com>
2014-10-15 15:50:56 +0100

    Added tests for video api endpoint

--- tests/test_video_api.py
+++ tests/test_video_api.py
@@ -1,13 +1,24 @@
 import pytest
 
-def test_wrong_part():
-    pass
+from youtube_api.client import VideoAPI
+from youtube_api.exceptions import *
 
-def test_wrong_filter():
-    pass
+from mock import patch
 
-def test_wrong_optional_params():
-    pass
+
+@pytest.mark.parametrize(('resource_filter', 'parts', 'optional_params', 'expected_exception'), [
+    ({'id': 1},            ('test',),    {},              PartNotAllowed),
+    ({'test': 1},          ('snippet',), {},              FilterNotAllowed),
+    ({'id': 1, 'test': 1}, ('snippet',), {},              OneFilterAllowed),
+    ({'id': 1},            ('snippet',), {'test': False}, OptionalParamNotAllowed)
+])
+def test_query_parameters_validation(resource_filter, parts, optional_params, expected_exception):
+    client = VideoAPI('api_key')
+    with pytest.raises(expected_exception):
+        client.digest_request_params(resource_filter, parts, optional_params)
+
 
 def test_quota():
-    pass
+    client = VideoAPI('api_key')
+    client.allowed_parts = {'test_key': 10, 'test_key2': 4, 'test_key3': 6}
+    assert client.calculate_quota(('test_key2', 'test_key3')) == 10
